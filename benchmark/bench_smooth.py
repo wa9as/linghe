cf887724aa9eeb7ef99b688ae7a57b1cf99da01a
violet.czx <violet.czx@antgroup.com>
2025-06-11 14:27:42 +0800

    minor fix
    Signed-off-by: violet.czx@antgroup.com <violet.czx>

--- benchmark/bench_smooth.py
+++ benchmark/bench_smooth.py
@@ -3,6 +3,7 @@ import torch
 from flops.quant.smooth.naive_smooth import *
 from flops.quant.smooth.reused_smooth import *
 from flops.quant.smooth.seperate_smooth import *
+
 from flops.utils.util import *
 from flops.utils.benchmark import benchmark_func
 
@@ -58,8 +59,8 @@ def benchmark_with_shape(shape):
     # ref_time = benchmark_func(fp16_update, y, x, n_repeat=n_repeat, ref_flops=batch_size*in_dim*out_dim*2)
     # benchmark_func(seperate_smooth_quant_update, y, x, n_repeat=n_repeat, ref_flops=batch_size*in_dim*out_dim*2, ref_time=ref_time)
 
-    ref_time = benchmark_func(fp16_f_and_b, x, w, y, n_repeat=n_repeat, ref_flops=batch_size*in_dim*out_dim*6)
-    benchmark_func(seperate_smooth_quant_f_and_b, x, w, y, n_repeat=n_repeat, ref_flops=batch_size*in_dim*out_dim*6, ref_time=ref_time)
+    ref_time = benchmark_func(fp16_f_and_b, x, w, y, n_repeat=n_repeat, ref_flops=M*K*N*6)
+    benchmark_func(seperate_smooth_quant_f_and_b, x, w, y, n_repeat=n_repeat, ref_flops=M*K*N*6, ref_time=ref_time)
 
     # benchmark_func(triton_slide_smooth_quant,x,xw_smooth_scale)
     # benchmark_func(triton_slide_smooth_quant,w,1/xw_smooth_scale)
@@ -83,23 +84,13 @@ def benchmark_with_shape(shape):
     # ref_time = benchmark_func(fp16_update, y, x, n_repeat=n_repeat, ref_flops=M*K*N*2)
     # benchmark_func(reused_smooth_quant_update, y, x_f8, xw_smooth_scale, x_s, n_repeat=n_repeat, ref_flops=M*K*N*2, ref_time=ref_time)
 
-<<<<<<< HEAD
-    ref_time = benchmark_func(fp16_f_and_b, x, w, y, n_repeat=n_repeat, ref_flops=batch_size*in_dim*out_dim*6)
-    benchmark_func(reused_smooth_quant_f_and_b, x, w, y, xw_smooth_scale, n_repeat=n_repeat, ref_flops=batch_size*in_dim*out_dim*6, ref_time=ref_time)
-
-=======
     # ref_time = benchmark_func(fp16_f_and_b, x, w, y, n_repeat=n_repeat, ref_flops=M*K*N*6)
     # benchmark_func(reused_smooth_quant_f_and_b, x, w, y, n_repeat=n_repeat, ref_flops=M*K*N*6, ref_time=ref_time)
->>>>>>> 1f289d55f4c0bbfc1d0f1be26a5520840a05847b
 
 
 
 
-<<<<<<< HEAD
-benchmark_with_shape([4096, 4096, 6144])
-=======
 benchmark_with_shape([2048, 8192, 2048])
->>>>>>> 1f289d55f4c0bbfc1d0f1be26a5520840a05847b
 
 # for shape in [[8192, 6144, 4096], [8192, 4096, 4096], [8192, 13312, 4096], [8192, 4096, 13312],
 #             [8192, 10240, 8192],[8192, 8192, 8192],[8192, 34048, 8192],[8192, 4096, 34048]]:
