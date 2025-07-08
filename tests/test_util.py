b3923e2fbb45e5d6641be36704fdb62492eefb65
nanxiao.zy <nanxiao.zy@antgroup.com>
2025-07-08 11:31:32 +0800

    add fp32 gemm for moe router

--- tests/test_util.py
+++ tests/test_util.py
@@ -10,12 +10,13 @@ from flops.utils.norm import *
 from flops.utils.benchmark import benchmark_func
 
 
-# M, N, K = 8192, 4096, 13312
+M, N, K = 8192, 4096, 8192
 # M, N, K = 4096, 4096, 6144
 # M, N, K = 4096, 4096, 4096
-M, N, K = 4096, 256, 8192
+# M, N, K = 4096, 256, 8192
 
-dtype = torch.float32
+# dtype = torch.float32
+dtype = torch.bfloat16
 device = 'cuda:0'
 n_repeat = 100
 
@@ -28,3 +29,6 @@ if False:
     xs = [torch.randn(N, dtype=dtype, device=device) for x in range(32)]
     benchmark_func(torch.stack, xs, 0, n_repeat=n_repeat, ref_bytes=N*32*8)
 
+if True:
+    x = torch.randn(M, K, dtype=dtype, device=device)
+    benchmark_func(lambda x:x.zero_(), x, n_repeat=n_repeat, ref_bytes=M*K*2)
