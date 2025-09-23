5a2c113b4b54b3161ac183774ca9ceab6eff0120
nanxiao.zy <nanxiao.zy@antgroup.com>
2025-09-23 14:17:52 +0800

    public

--- README.md
+++ README.md
@@ -4,16 +4,32 @@
 
   
 <p align="center">
-   A high-performance kernels to accelerate LLM inference and training.
+   A collection of high-performance kernels for LLM training.
 </p>
 
 
+
+## Roadmap ##
+
+- All more shapes support and optional auto tuning with our kernels.
+- Release our fp8 training kernels beyond blockwise quantization.
+
 ## *News or Update* 🔥
 
-- [2025/07] We implement multiple kernels for fp8 training with smooth quant.
+- [2025/07] We implement multiple kernels for fp8 training with `Megatron-LM` blockwise quantization. 
 
 
 ## Introduction
 
-Our repo, FLOPS (short for Flood OPS), is designed for LLM inference and training.
+Our repo, FLOPS, is designed for LLM training. It provides 3 main categories of kernels:
+
+- ** Fused quantization kernels **: fuse quantization with previous layer, e.g., RMS norm and Silu.
+- ** Memory-friendly kernels **: use dtype cast in kernels instead of casting out kernels, e.g., softmax cross entropy and moe router gemm.
+- ** Fused multi-operation kernels **: fuse multiple IO-itensive operations, e.g., ROPE with qk-norm and transpose, permute and padding, group RMS norm with sigmoid gate.
+
+
+
+
+
+
 
--- flops/utils/norm.py
+++ flops/utils/norm.py
@@ -117,50 +117,6 @@ def triton_rms_norm_backward(grad_output, x, w, eps=1e-6):
     return dx, tmp_dw.sum(dim=0).to(w.dtype)
 
 
-@triton.jit
-def rms_norm_and_smooth_quant_forward_kernel(x_ptr, weight_ptr, smooth_scale_ptr,
-                                      out_ptr, scale_ptr, max_ptr, rms_ptr, 
-                                      eps,
-                                      M, 
-                                      T, 
-                                      N: tl.constexpr, 
-                                      W: tl.constexpr,
-                                      CALIBRATE: tl.constexpr,
-                                      OUTPUT: tl.constexpr,
-                                      ROUND: tl.constexpr):
-    pid = tl.program_id(axis=0)
-    # row-wise read, row-wise write
-    weight = tl.load(weight_ptr + tl.arange(0, N)).to(tl.float32)[None, :]
-    smooth_scale = tl.load(smooth_scale_ptr + tl.arange(0, N))[None, :]
-    smooth_scale = 1.0 / tl.maximum(smooth_scale, 1e-30)
-    if CALIBRATE:
-        # triton 3.3.1 has bug with N = 2048 and calibrate=True 
-        maxs = tl.zeros((N, ), dtype=tl.float32)
-    offs = pid * W * T * N + tl.arange(0, W)[:, None] * N + tl.arange(0, N)[
-                                                            None, :]
-    for i in range(T):
-        indices = pid * W * T + i * W + tl.arange(0, W)
-        x = tl.load(x_ptr + offs, mask=indices[:, None] < M).to(tl.float32)
-        rms = 1/tl.sqrt(tl.sum(x * x, axis=1) / N + eps)
-        if OUTPUT:
-            tl.store(rms_ptr + indices, rms, mask=indices < M)
-        x = x * rms[:, None] * weight
-
-        if CALIBRATE:
-            maxs = tl.maximum(maxs, tl.max(tl.abs(x),0))
-
-        x = x * smooth_scale
-        scale = tl.maximum(tl.max(tl.abs(x), 1) / 448.0, 1e-30)
-        if ROUND:
-            scale = tl.exp2(tl.ceil(tl.log2(scale)))
-        q = (x / scale[:, None]).to(out_ptr.dtype.element_ty)
-        tl.store(scale_ptr + indices, scale, mask=indices < M)
-        tl.store(out_ptr + offs, q, mask=indices[:, None] < M)
-        offs += N * W
-
-    if CALIBRATE:
-        tl.store(max_ptr + pid * N + tl.arange(0, N), maxs)
-
 # output non-transposed and transposed together
 # should used with batchsize >= 16384
 @triton.jit
@@ -298,57 +254,6 @@ def rms_norm_and_block_quant_forward_t_kernel(x_ptr,
     x = (tl.trans(x/scale)).to(transpose_output_ptr.dtype.element_ty)
     tl.store(transpose_output_ptr + toffs, x, mask=indices[None, :] < M)
 
-
-
-
-# rms is used for moe routing, it is stored as 1/rms
-def triton_rms_norm_and_smooth_quant_forward(x, weight, smooth_scale=None, eps=1e-6,
-                                      out=None, scale=None, rms=None, calibrate=False,
-                                      output_rms=False, round_scale=False):
-    # row-wise read, row-wise write
-    M, N = x.shape
-    assert N <= 8192 and 8192 % N == 0
-    device = x.device
-
-    if out is None:
-        out = torch.empty((M, N), device=device, dtype=torch.float8_e4m3fn)
-
-    if scale is None:
-        scale = torch.empty((M,), device=device, dtype=torch.float32)
-    W = 8192 // N
-    T = 8 if M // W >= 4096 else 4
-    assert M % (T * W) == 0
-    g = M // (T*W)
-    if calibrate:
-        maxs = torch.empty((g, N), dtype=torch.float32, device=device)
-    else:
-        maxs = None
-    if output_rms and rms is None:
-        rms = torch.empty((M,), dtype=torch.float32, device=device)
-    grid = (g,)
-    rms_norm_and_smooth_quant_forward_kernel[grid](
-        x,
-        weight,
-        smooth_scale,
-        out,
-        scale,
-        maxs,
-        rms,
-        eps,
-        M,
-        T,
-        N,
-        W,
-        calibrate,
-        output_rms,
-        round_scale,
-        num_stages=3,
-        num_warps=2 if N == 2048 else 4
-    )
-    if calibrate:
-        maxs = maxs.amax(0)
-    
-    return out, scale, maxs, rms
 
 
 def triton_rms_norm_and_block_quant_forward(x, weight, eps=1e-6,
