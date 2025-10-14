1b9622f4568c32471f43027b4fe53f6e55c7e173
南霄 <nanxiao.zy@antgroup.com>
2025-10-14 19:24:25 +0800

    revise email

---
+++ t.sh
@@ -0,0 +1,14 @@
+git filter-branch --env-filter '
+OLD_EMAIL="violet.czx"
+CORRECT_NAME="violet.czx"
+CORRECT_EMAIL="violet.czx@antgroup.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]; then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]; then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --all
